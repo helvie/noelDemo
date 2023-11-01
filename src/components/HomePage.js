aaede13625e9272b0422e623fa4baa424d7f5bc7
Sylvie Helin <sylviepil1l1@gmail.com>
2023-11-01 15:47:11 +0100

    modifs faites sur place avec Armel récup bdd

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -102,7 +102,7 @@ function HomePage() {
     // }, []);
     useEffect(() => {
     
-        fetch("https://noel.helvie.fr/api/gettoken.php", {
+        fetch("http://noel.helvie.fr/api/gettoken.php", {
             headers: envVariables,
         })
         .then(response => response.text())
@@ -110,7 +110,7 @@ function HomePage() {
             console.log(tokenData)
                 setToken(tokenData);
     
-            fetch("https://noel.helvie.fr/api/getlistesetcadeaux.php", {
+            fetch("http://noel.helvie.fr/api/getlistesetcadeaux.php", {
                 headers: {
                     "user-name":"moarmel",
                     "app-name":"NoelTan",
