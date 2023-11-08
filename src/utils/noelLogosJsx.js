642a82b6f097ff0a2321d8c821ce47c361ba792f
Sylvie Helin <sylviepil1l1@gmail.com>
2023-11-08 18:55:26 +0100

    login et mdp ok

--- src/utils/noelLogosJsx.js
+++ src/utils/noelLogosJsx.js
@@ -1,25 +1,25 @@
 const noelLogos = [
-    <p>&amp;</p>,
-    <p>&lt;</p>,
-    <p>&brvbar;</p>,
-    <p>&dagger;</p>,
-    <p>&sect;</p>,
-    <p>&copy;</p>,
-    <p>&laquo;</p>,
-    <p>&amp;</p>,
-    <p>&lt;</p>,
-    <p>&brvbar;</p>,
-    <p>&dagger;</p>,
-    <p>&sect;</p>,
-    <p>&copy;</p>,
-    <p>&laquo;</p>,
-    <p>&amp;</p>,
-    <p>&lt;</p>,
-    <p>&brvbar;</p>,
+    <p key="logo1">&amp;</p>,
+    <p key="logo2">&lt;</p>,
+    <p key="logo3">&brvbar;</p>,
+    <p key="logo4">&dagger;</p>,
+    <p key="logo5">&sect;</p>,
+    <p key="logo6">&copy;</p>,
+    <p key="logo7">&laquo;</p>,
+    <p key="logo8">&amp;</p>,
+    <p key="logo9">&lt;</p>,
+    <p key="logo10">&brvbar;</p>,
+    <p key="logo11">&dagger;</p>,
+    <p key="logo12">&sect;</p>,
+    <p key="logo13">&copy;</p>,
+    <p key="logo14">&laquo;</p>,
+    <p key="logo15">&amp;</p>,
+    <p key="logo16">&lt;</p>,
+    <p key="logo17">&brvbar;</p>,
   ];
 
   const noelLogosJsx = noelLogos.map((data, i) => (
-    <div key={i}> {data}</div>
+     data
   ));
   
   export { noelLogosJsx };
